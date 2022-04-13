066375d471d7bd5e53a11f7359c15bd80a2155cf
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2022-04-13 22:58:01 +0200

    Intégration simulation

--- HELBPark.java
+++ HELBPark.java
@@ -3,5 +3,6 @@ public class HELBPark
     public static void main(String[] args) 
     {
         System.out.println("Hello !");
+        LogicPark.start();
     }
 }
---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+public class Vehicle
+{
+    private String type;
+    private String plate;
+
+    public Vehicle(String type, String plate)
+    {
+        this.type = type;
+        this.plate = plate;
+    }
+
+    public String getType() { return type; }
+    public String getPlate() { return plate; }
+}
